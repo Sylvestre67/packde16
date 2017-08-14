e881f89916963d46555c218457b167d2e7c73fba
Sylvestre Gug <sylvestre.gug@gmail.com>
2017-08-13 20:20:47 -0400

    GA

--- p216/src/index.js
+++ p216/src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const ReactGA = require('react-ga');
+ReactGA.initialize('UA-104523856-1');
+
 ReactDOM.render(<App step={1}/>, document.getElementById('root'));
 registerServiceWorker();
