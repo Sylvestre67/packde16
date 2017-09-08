d1db2131ce71119e52defd9b30cef993b87bf523
Sylvestre Gug <sylvestre.gug@gmail.com>
2017-09-08 12:51:54 -0400

    credit alignement

--- p216/src/component/Track.js
+++ p216/src/component/Track.js
@@ -20,17 +20,19 @@ class Track extends Component{
 				</div>
 		</div>;
 
-		const credit = <div className="credit">
-						<p>All songs written by Kozmo, arranged and mixed by Darius Studio.</p>
-						<p>Mastering by Darius Studio</p>
-						<p>Photography & Artwork: Bartosch Salamanski</p>
-						<p>Web: <a href="https://github.com/sylvestre67" target="_blank">Sylvestre</a></p>
-						<p>Recording: Darius Studio</p>
-						<p>Foodtruck:
-							<a href="http://auptitsawyer.fr/" target="_blank">
-								Au Ptit Sawyer
-							</a>
-						</p>
+		const credit = <div className="credit-wrap">
+						<div className="credit">
+							<p>All songs written by Kozmo, arranged and mixed by Darius Studio.</p>
+							<p>Mastering by Darius Studio</p>
+							<p>Photography & Artwork: Bartosch Salamanski</p>
+							<p>Web: <a href="https://github.com/sylvestre67" target="_blank">Sylvestre</a></p>
+							<p>Recording: Darius Studio</p>
+							<p>Foodtruck:
+								<a href="http://auptitsawyer.fr/" target="_blank">
+									Au Ptit Sawyer
+								</a>
+							</p>
+						</div>
 					</div>;
 
 		return (
