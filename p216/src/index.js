c37a49dc43195d47787b10453ba72d8ded2d3093
Sylvestre Gug <sylvestre.gug@gmail.com>
2017-09-08 13:05:21 -0400

    avatars for 3-4

--- p216/src/index.js
+++ p216/src/index.js
@@ -25,10 +25,15 @@ import je_ne_comprends_pas from './img/je_ne_comprends_pas.png';
 import bombes from './img/bombes.png';
 import caisse from './img/caisse.png';
 import burger from './img/burger.png';
-import phone from './img/phone.png';
+
 import ride from './img/ride.png';
 import dinner from './img/dinner.png';
 import toy from './img/toy.png';
+import check from './img/check.png';
+import comptoir from './img/comptoir.png';
+
+// Wrong orientation --- DO NOT USE
+// import phone from './img/phone.png';
 
 const listOfImages = [intro, ma_gueule, pack_de_koz, mon_ivresse, visage,
 	moment, pack_de_lautrec, sugar_man, cremant_redbull,pack_de_paul, pack_de_nyls,
@@ -68,14 +73,14 @@ const tracks = [
 		url: 'https://s3.eu-central-1.amazonaws.com/pack-de-16/4.%23MONIVRESSE.mp3',
 		title: 'Mon Ivresse',
 		feat: '',
-		avatar: toy
+		avatar: bombes
 	},
 	{
 		id: uuidv4(),
 		url: 'https://s3.eu-central-1.amazonaws.com/pack-de-16/5.%23VISAGE.mp3',
 		title: 'Visage',
 		feat: 'JESERS',
-		avatar: toy
+		avatar: dinner
 	},
 	{
 		id: uuidv4(),
@@ -89,14 +94,14 @@ const tracks = [
 		url: 'https://s3.eu-central-1.amazonaws.com/pack-de-16/7.%23PACKDELAUTREC.mp3',
 		title: 'Pack de Lautrec',
 		feat: 'LAUTREC',
-		avatar: toy
+		avatar: check
 	},
 	{
 		id: uuidv4(),
 		url: 'https://s3.eu-central-1.amazonaws.com/pack-de-16/8.%23SUGARMAN.mp3',
 		title: 'Sugar Man',
 		feat: 'YANNICK EICHERT & AKASHA',
-		avatar: toy
+		avatar: comptoir
 	},
 	{
 		id: uuidv4(),
