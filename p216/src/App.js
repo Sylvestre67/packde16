f6fc001c3e00ddc158d200663a83116c484edc36
Sylvestre Gug <sylvestre.gug@gmail.com>
2018-02-03 10:29:48 -0500

    Make a prod build, fixed MediaCommand PlayPause method

--- p216/src/App.js
+++ p216/src/App.js
@@ -16,120 +16,122 @@ import './App.css';
 injectTapEventPlugin();
 
 class App extends Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.renderIllustrations = this.renderIllustrations.bind(this);
-		this.prevTrack = this.prevTrack.bind(this);
-		this.nextTrack = this.nextTrack.bind(this);
-		this._playNextOnFinish= this._playNextOnFinish.bind(this);
+        this.renderIllustrations = this.renderIllustrations.bind(this);
+        this.prevTrack = this.prevTrack.bind(this);
+        this.nextTrack = this.nextTrack.bind(this);
+        this._playNextOnFinish = this._playNextOnFinish.bind(this);
 
-		this.state = {
-				step: 0,
-				audio: this.props.tracks[0]
-			};
-	}
+        this.state = {
+            step: 0,
+            audio: this.props.tracks[0],
+        };
+    }
 
-	componentDidMount(){
-		if(window.location.hash !== ''){
-			let step = +(window.location.hash.replace('#',''));
-			this.refs.reactSwipe.slide(step, 250);
-			this._updateLocationHash(step);
-		}else{
-			this._updateLocationHash(0);
-		}
-	}
+    componentDidMount() {
+        if (window.location.hash !== '') {
+            let step = +window.location.hash.replace('#', '');
+            this.refs.reactSwipe.slide(step, 250);
+            this._updateLocationHash(step);
+        } else {
+            this._updateLocationHash(0);
+        }
+    }
 
-	_updateLocationHash(step){
-		window.location.hash = step;
-		let track = this.props.tracks[step];
-		window.ga('send', 'event', 'song', 'play', track.title);
-	}
+    _updateLocationHash(step) {
+        window.location.hash = step;
+        let track = this.props.tracks[step];
+        window.ga('send', 'event', 'song', 'play', track.title);
+    }
 
-	_playNextOnFinish(){
-		if(this.state.step <= this.props.tracks.length){
-			this.nextTrack();
-		}
-	}
+    _playNextOnFinish() {
+        if (this.state.step <= this.props.tracks.length) {
+            this.nextTrack();
+        }
+    }
 
-	nextTrack() {
-		this.refs.reactSwipe.next();
+    nextTrack() {
+        this.refs.reactSwipe.next();
 
-		this.setState((prevState) => {
+        this.setState(prevState => {
+            this._updateLocationHash(this.refs.reactSwipe.getPos());
 
-			this._updateLocationHash( this.refs.reactSwipe.getPos());
+            return {
+                step: this.refs.reactSwipe.getPos(),
+                audio: this.props.tracks[this.refs.reactSwipe.getPos()],
+            };
+        });
+    }
 
-			return {
-				step: this.refs.reactSwipe.getPos(),
-				audio: this.props.tracks[this.refs.reactSwipe.getPos()]
-			}
-		});
-	}
+    prevTrack() {
+        this.refs.reactSwipe.prev();
 
-	prevTrack() {
-		this.refs.reactSwipe.prev();
+        this.setState(prevState => {
+            this._updateLocationHash(this.refs.reactSwipe.getPos());
 
-		this.setState((prevState) => {
+            return {
+                step: this.refs.reactSwipe.getPos(),
+                audio: this.props.tracks[this.refs.reactSwipe.getPos()],
+            };
+        });
+    }
 
-			this._updateLocationHash( this.refs.reactSwipe.getPos());
+    renderIllustrations() {
+        return this.props.listOfImages.map((img, index) => {
+            return <Illustration key={index} img={img} />;
+        });
+    }
 
-			return {
-				step: this.refs.reactSwipe.getPos(),
-				audio: this.props.tracks[this.refs.reactSwipe.getPos()]
-			}
-		});
+    renderReactSwipe() {
+        return (
+            <ReactSwipe
+                ref="reactSwipe"
+                className="illustrations"
+                swipeOptions={{
+                    speed: 500,
+                    disableScroll: true,
+                    stopPropagation: true,
+                    continuous: false,
+                    transitionEnd: (index, elem) => {
+                        this.setState(() => {
+                            return {
+                                step: index,
+                                audio: this.props.tracks[index],
+                            };
+                        });
+                    },
+                }}
+            >
+                {this.renderIllustrations()}
+            </ReactSwipe>
+        );
+    }
 
-	}
-
-	renderIllustrations(){
-		return  this.props.listOfImages.map((img,index) => {
-			return <Illustration key={index} img={img}/>
-		});
-	}
-
-	renderReactSwipe(){
-		return <ReactSwipe ref="reactSwipe"
-		                   className="illustrations"
-		                   swipeOptions={{
-			                   speed: 500,
-			                   disableScroll: true,
-			                   stopPropagation: true,
-			                   continuous: false,
-			                   transitionEnd: (index, elem) => {
-				                   this.setState(() => {
-					                   return {
-						                   step: index,
-						                   audio: this.props.tracks[index]
-					                   }
-				                   });
-			                   }
-		                   }}>
-			{this.renderIllustrations()}
-		</ReactSwipe>;
-	}
-
-	render() {
-		return (
-			<MuiThemeProvider>
-				<div className="App"
-				     style={{'backgroundImage': ['url(', this.props.listOfImages[0], ')'].join('')}}>
-					{this.renderReactSwipe()}
-					<Track {...this.state} />
-					<OtherPlatform />
-					<NextPrevious numberOfTracks={this.props.listOfImages.length}
-						step={this.state.step}
-						onNext={this.nextTrack}
-						onPrev={this.prevTrack} />
-					<MediaPlayer playNextOnFinish={this._playNextOnFinish} audio={this.state.audio} />
-				</div>
-			</MuiThemeProvider>
-		);
-	}
+    render() {
+        return (
+            <MuiThemeProvider>
+                <div className="App" style={{ backgroundImage: ['url(', this.props.listOfImages[0], ')'].join('') }}>
+                    {this.renderReactSwipe()}
+                    <Track {...this.state} />
+                    <OtherPlatform />
+                    <NextPrevious
+                        numberOfTracks={this.props.listOfImages.length}
+                        step={this.state.step}
+                        onNext={this.nextTrack}
+                        onPrev={this.prevTrack}
+                    />
+                    <MediaPlayer playNextOnFinish={this._playNextOnFinish} audio={this.state.audio} />
+                </div>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 App.propTypes = {
-	tracks: PropTypes.arrayOf(PropTypes.object).isRequired,
-	listOfImages: PropTypes.arrayOf(PropTypes.string).isRequired
+    tracks: PropTypes.arrayOf(PropTypes.object).isRequired,
+    listOfImages: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default App;
--- p216/src/component/MediaCommand.js
+++ p216/src/component/MediaCommand.js
@@ -11,111 +11,69 @@ import CircularProgress from 'material-ui/CircularProgress';
 import { withMediaProps } from 'react-media-player';
 
 const styles = {
-	smallIcon: {
-		width: 36,
-		height: 36,
-	},
-	small: {
-		width: 72,
-		height: 72,
-		padding: 16,
-	},
-	mediumIcon: {
-		width: 48,
-		height: 48,
-	},
-	medium: {
-		width: 96,
-		height: 96,
-		padding: 24,
-	}
+    smallIcon: {
+        width: 36,
+        height: 36,
+    },
+    small: {
+        width: 72,
+        height: 72,
+        padding: 16,
+    },
+    mediumIcon: {
+        width: 48,
+        height: 48,
+    },
+    medium: {
+        width: 96,
+        height: 96,
+        padding: 24,
+    },
 };
 
-class MediaCommand extends Component{
-	constructor(props){
-		super(props);
-		this.state = { hasBeenReset: false };
-	}
+class MediaCommand extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasBeenReset: false };
+    }
 
-	// shouldComponentUpdate({media}) {
-	// 	if(media.currentTime / media.duration === 1 && !media.isPlaying){
-	// 		console.log('Playing Next');
-	// 		console.log(this.state);
-	// 		if(!this.state.hasBeenReset){
-	// 			this.setState((prevProps) => { return { hasBeenReset : true } });
-	//
-	// 			setTimeout(() => {
-	// 				this.props.playNextOnFinish();
-	// 				this.setState((prevProps) => {
-	// 					return { hasBeenReset : false }
-	// 				});
-	// 			},500);
-	// 		}
-	//
-	// 	}
-	// 	return true
-	// }
+    componentDidMount() {
+        this.props.media.playPause();
+    }
 
-	// onUpdate({media}){
-	// 	// console.log(this.props.media.currentTime / this.props.media.duration);
-	//
-	// 	if(this.props.media.currentTime / this.props.media.duration === 1){
-	// 		debugger;
-	//
-	// 		this.state.playNextOnFinish();
-	// 	}
-	//
-	// }
+    _renderPlayIcon() {
+        let icon;
 
-	componentDidUpdate(prevProps, prevState){}
+        if (this.props.media.currentTime > 0.1 && this.props.media.isPlaying) {
+            icon = <AvPause />;
+        } else if (this.props.media.currentTime === 0 && this.props.media.isPlaying) {
+            icon = <CircularProgress color="black" />;
+        } else {
+            icon = <AvPlayArrow />;
+        }
 
-	componentDidMount(){
-		this._handlePlayPause();
-	}
+        return icon;
+    }
 
-	_handlePlayPause = () => {
-		this.props.media.playPause()
-	};
-
-	_renderPlayIcon(){
-		let icon;
-
-		if(this.props.media.currentTime > 0.1
-			&& this.props.media.isPlaying){
-			icon = <AvPause />
-		}
-
-		else if(this.props.media.currentTime === 0
-			&& this.props.media.isPlaying){
-			icon = <CircularProgress color='black'/>
-		}
-
-		else{
-			icon = <AvPlayArrow />;
-		}
-
-		return icon
-	}
-
-	render() {
-		return (
-				<div className="media-command">
-					<IconButton className="media-btn"
-								style={styles.medium}
-								touch={true}
-								iconStyle={styles.mediumIcon}
-								onClick={this._handlePlayPause}>
-						{this._renderPlayIcon()}
-					</IconButton>
-				</div>
-		);
-	}
+    render() {
+        return (
+            <div className="media-command">
+                <IconButton
+                    className="media-btn"
+                    style={styles.medium}
+                    touch={true}
+                    iconStyle={styles.mediumIcon}
+                    onClick={this.props.media.playPause}
+                >
+                    {this._renderPlayIcon()}
+                </IconButton>
+            </div>
+        );
+    }
 }
 
-// {(this.props.media.isLoading) ? <CircularProgress color='black'/> : this._renderPlayIcon()}
-
 MediaCommand.propTypes = {
-	media: PropTypes.object.isRequired
+    media: PropTypes.object.isRequired,
 };
 
 export default withMediaProps(MediaCommand);
